f3b27ad5aa397a56e2ed815b59a33e8bfba55637
HaoboGu <haobogu@outlook.com>
2024-02-03 14:26:09 +0800

    minimal demo
    Signed-off-by: HaoboGu <haobogu@outlook.com>

---
+++ src/main.rs
@@ -0,0 +1,286 @@
+#![no_std]
+#![no_main]
+
+use core::sync::atomic::{AtomicBool, Ordering};
+
+use defmt::{panic, *};
+use embassy_executor::Spawner;
+use embassy_stm32::peripherals::USB_OTG_HS;
+use embassy_stm32::time::Hertz;
+use embassy_stm32::usb_otg::Driver;
+use embassy_stm32::{bind_interrupts, peripherals, usb_otg, Config};
+use embassy_time::Timer;
+use embassy_usb::class::hid::{HidReaderWriter, ReportId, RequestHandler, State};
+use embassy_usb::control::OutResponse;
+use embassy_usb::driver::EndpointError;
+use embassy_usb::{Builder, Handler};
+use futures::future::join;
+use static_cell::StaticCell;
+use usbd_hid::descriptor::{
+    generator_prelude::*, MediaKeyboardReport}
+;
+use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    OTG_HS => usb_otg::InterruptHandler<peripherals::USB_OTG_HS>;
+});
+
+#[gen_hid_descriptor(
+    (report_id = 0x01, collection = APPLICATION, usage_page = CONSUMER, usage = CONSUMER_CONTROL) = {
+        (usage_page = CONSUMER, usage_min = 0x00, usage_max = 0x514) = {
+            #[item_settings data,array,absolute,not_null] media_usage_id=input;
+        };
+    },
+    (report_id = 0x02, collection = APPLICATION, usage_page = GENERIC_DESKTOP, usage = SYSTEM_CONTROL) = {
+        (usage_min = 0x81, usage_max = 0xB7, logical_min = 1) = {
+            #[item_settings data,array,absolute,not_null] system_usage_id=input;
+        };
+    }
+)]
+#[derive(Default)]
+pub struct CompositeReport {
+    pub(crate) buttons: u8,
+    pub(crate) x: i8,
+    pub(crate) y: i8,
+    pub(crate) wheel: i8, // Scroll down (negative) or up (positive) this many units
+    pub(crate) pan: i8,   // Scroll left (negative) or right (positive) this many units
+    pub(crate) media_usage_id: u16,
+    pub(crate) system_usage_id: u8,
+}
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("Hello World!");
+
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hsi = Some(HSIPrescaler::DIV1);
+        config.rcc.csi = true;
+        config.rcc.hsi48 = Some(Hsi48Config {
+            sync_from_usb: true,
+        }); // needed for USB
+        config.rcc.pll1 = Some(Pll {
+            source: PllSource::HSI,
+            prediv: PllPreDiv::DIV4,
+            mul: PllMul::MUL50,
+            divp: Some(PllDiv::DIV2),
+            divq: None,
+            divr: None,
+        });
+        config.rcc.sys = Sysclk::PLL1_P; // 400 Mhz
+        config.rcc.ahb_pre = AHBPrescaler::DIV2; // 200 Mhz
+        config.rcc.apb1_pre = APBPrescaler::DIV2; // 100 Mhz
+        config.rcc.apb2_pre = APBPrescaler::DIV2; // 100 Mhz
+        config.rcc.apb3_pre = APBPrescaler::DIV2; // 100 Mhz
+        config.rcc.apb4_pre = APBPrescaler::DIV2; // 100 Mhz
+        config.rcc.voltage_scale = VoltageScale::Scale1;
+    }
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hsi = Some(HSIPrescaler::DIV1);
+        config.rcc.csi = true;
+        // Needed for USB
+        config.rcc.hsi48 = Some(Hsi48Config {
+            sync_from_usb: true,
+        });
+        // External oscillator 25MHZ
+        config.rcc.hse = Some(Hse {
+            freq: Hertz(25_000_000),
+            mode: HseMode::Oscillator,
+        });
+        config.rcc.pll1 = Some(Pll {
+            source: PllSource::HSE,
+            prediv: PllPreDiv::DIV5,
+            mul: PllMul::MUL112,
+            divp: Some(PllDiv::DIV2),
+            divq: Some(PllDiv::DIV2),
+            divr: Some(PllDiv::DIV2),
+        });
+        config.rcc.sys = Sysclk::PLL1_P;
+        config.rcc.ahb_pre = AHBPrescaler::DIV2;
+        config.rcc.apb1_pre = APBPrescaler::DIV2;
+        config.rcc.apb2_pre = APBPrescaler::DIV2;
+        config.rcc.apb3_pre = APBPrescaler::DIV2;
+        config.rcc.apb4_pre = APBPrescaler::DIV2;
+        config.rcc.voltage_scale = VoltageScale::Scale0;
+    };
+    let p = embassy_stm32::init(config);
+
+    // Usb config
+    static EP_OUT_BUFFER: StaticCell<[u8; 1024]> = StaticCell::new();
+    let mut usb_config = embassy_stm32::usb_otg::Config::default();
+    usb_config.vbus_detection = false;
+    let driver = Driver::new_fs(
+        p.USB_OTG_HS,
+        Irqs,
+        p.PA12,
+        p.PA11,
+        &mut EP_OUT_BUFFER.init([0; 1024])[..],
+        usb_config,
+    );
+
+    // Create embassy-usb Config
+    let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("Embassy");
+    config.product = Some("USB-serial example");
+    config.serial_number = Some("12345678");
+
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    static DEVICE_DESC: StaticCell<[u8; 256]> = StaticCell::new();
+    static CONFIG_DESC: StaticCell<[u8; 256]> = StaticCell::new();
+    static BOS_DESC: StaticCell<[u8; 256]> = StaticCell::new();
+    static MSOS_DESC: StaticCell<[u8; 128]> = StaticCell::new();
+    static CONTROL_BUF: StaticCell<[u8; 128]> = StaticCell::new();
+
+    // UsbDevice builder
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut DEVICE_DESC.init([0; 256])[..],
+        &mut CONFIG_DESC.init([0; 256])[..],
+        &mut BOS_DESC.init([0; 256])[..],
+        &mut MSOS_DESC.init([0; 128])[..],
+        &mut CONTROL_BUF.init([0; 128])[..],
+    );
+
+    static DEVICE_HANDLER: StaticCell<MyDeviceHandler> = StaticCell::new();
+    builder.handler(DEVICE_HANDLER.init(MyDeviceHandler::new()));
+
+    // Create classes on the builder.
+    static REQUEST_HANDLER: MyRequestHandler = MyRequestHandler {};
+
+    let other_hid_config = embassy_usb::class::hid::Config {
+        // report_descriptor: MediaKeyboardReport::desc(),  // <== This line works well
+        report_descriptor: CompositeReport::desc(), //  <== This line makes HidReaderWriter block
+        request_handler: Some(&REQUEST_HANDLER),
+        poll_ms: 60,
+        max_packet_size: 64,
+    };
+    static OTHER_HID_STATE: StaticCell<State> = StaticCell::new();
+    let mut other_hid: HidReaderWriter<'_, Driver<'_, USB_OTG_HS>, 1, 9> = HidReaderWriter::new(
+        &mut builder,
+        OTHER_HID_STATE.init(State::new()),
+        other_hid_config,
+    );
+
+    // Build the builder.
+    let mut usb = builder.build();
+
+    // Run the USB device.
+    let usb_fut = usb.run();
+
+    let send_fut = async {
+        let mut cnt = 0;
+        loop {
+            cnt += 1;
+            info!("loop {}", cnt);
+            let data: [u8; 3] = [0x01, 0x83, 0x00];
+            match other_hid.write(&data).await {
+                Ok(()) => {}
+                Err(e) => error!("Send other report error: {}", e),
+            };
+            Timer::after_secs(1).await;
+        }
+    };
+
+    // Run everything concurrently.
+    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
+    join(usb_fut, send_fut).await;
+}
+
+struct Disconnected {}
+
+impl From<EndpointError> for Disconnected {
+    fn from(val: EndpointError) -> Self {
+        match val {
+            EndpointError::BufferOverflow => panic!("Buffer overflow"),
+            EndpointError::Disabled => Disconnected {},
+        }
+    }
+}
+
+struct MyRequestHandler {}
+
+impl RequestHandler for MyRequestHandler {
+    fn get_report(&self, id: ReportId, _buf: &mut [u8]) -> Option<usize> {
+        info!("Get report for {:?}", id);
+        None
+    }
+
+    fn set_report(&self, id: ReportId, data: &[u8]) -> OutResponse {
+        info!("Set report for {:?}: {=[u8]}", id, data);
+        OutResponse::Accepted
+    }
+
+    fn set_idle_ms(&self, id: Option<ReportId>, dur: u32) {
+        info!("Set idle rate for {:?} to {:?}", id, dur);
+    }
+
+    fn get_idle_ms(&self, id: Option<ReportId>) -> Option<u32> {
+        info!("Get idle rate for {:?}", id);
+        None
+    }
+}
+
+struct MyDeviceHandler {
+    configured: AtomicBool,
+}
+
+impl MyDeviceHandler {
+    fn new() -> Self {
+        MyDeviceHandler {
+            configured: AtomicBool::new(false),
+        }
+    }
+}
+static SUSPENDED: AtomicBool = AtomicBool::new(false);
+
+impl Handler for MyDeviceHandler {
+    fn enabled(&mut self, enabled: bool) {
+        self.configured.store(false, Ordering::Relaxed);
+        SUSPENDED.store(false, Ordering::Release);
+        if enabled {
+            info!("Device enabled");
+        } else {
+            info!("Device disabled");
+        }
+    }
+
+    fn reset(&mut self) {
+        self.configured.store(false, Ordering::Relaxed);
+        info!("Bus reset, the Vbus current limit is 100mA");
+    }
+
+    fn addressed(&mut self, addr: u8) {
+        self.configured.store(false, Ordering::Relaxed);
+        info!("USB address set to: {}", addr);
+    }
+
+    fn configured(&mut self, configured: bool) {
+        self.configured.store(configured, Ordering::Relaxed);
+        if configured {
+            info!(
+                "Device configured, it may now draw up to the configured current limit from Vbus."
+            )
+        } else {
+            info!("Device is no longer configured, the Vbus current limit is 100mA.");
+        }
+    }
+
+    fn suspended(&mut self, suspended: bool) {
+        if suspended {
+            info!("Device suspended, the Vbus current limit is 500µA (or 2.5mA for high-power devices with remote wakeup enabled).");
+            SUSPENDED.store(true, Ordering::Release);
+        } else {
+            SUSPENDED.store(false, Ordering::Release);
+            if self.configured.load(Ordering::Relaxed) {
+                info!(
+                    "Device resumed, it may now draw up to the configured current limit from Vbus"
+                );
+            } else {
+                info!("Device resumed, the Vbus current limit is 100mA");
+            }
+        }
+    }
+}
